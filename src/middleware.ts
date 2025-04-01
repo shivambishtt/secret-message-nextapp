7e6847969f3243919fed7804827820ddefbe2f80
Shivam Bisht <rajshivam2745@gmail.com>
2025-04-01 22:59:08 +0530

    middleware fixes add

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,6 +15,10 @@ export async function middleware(request: NextRequest) {
     )) {
         return NextResponse.redirect(new URL('/dashboard', request.url))
     }
+    if (!token && url.pathname.startsWith("/dashboard")) {
+        return NextResponse.redirect(new URL("/signin", request.url))
+
+    }
 }
 
 export const config = {
